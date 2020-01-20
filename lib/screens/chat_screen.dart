17d4b5d8487eb7f563f5b95cd63a7103771f6b88
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2020-01-20 17:22:13 -0500

    adding routes and more

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flash_chat_flutter/constants.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: null,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.close),
+            onPressed: () {},
+          )
+        ],
+        title: Text('⚡️ Chat'),
+        backgroundColor: Colors.lightBlueAccent,
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              decoration: kMessageContainerDecoration,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      onChanged: (value) {},
+                      decoration: kMessageTextFieldDecoration,
+                    ),
+                  ),
+                  FlatButton(
+                      onPressed: () {},
+                      child: Text('Send', style: kSendButtonTextStyle))
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
