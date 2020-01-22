094866d9a8bcc1d69c317601094510d173121bc0
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2020-01-22 15:31:31 -0500

    adding spinner

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -5,6 +5,7 @@ import 'package:flash_chat_flutter/widgets/email_text_field.dart';
 import 'package:flash_chat_flutter/widgets/hero_logo.dart';
 import 'package:flash_chat_flutter/widgets/password_text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String id = 'login_screen';
@@ -14,6 +15,7 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   final _auth = FirebaseAuth.instance;
+  bool showSpinner = false;
   String password;
   String email;
 
@@ -21,38 +23,47 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            HeroLogo(imageHeight: 250),
-            SizedBox(height: 48.0),
-            EmailTextField(onChangedValue: (value) {
-              email = value;
-            }),
-            SizedBox(height: 8.0),
-            PasswordTextField(onChangedValue: (value) {
-              password = value;
-            }),
-            SizedBox(height: 24.0),
-            BottonsLogin(
-              function: () async {
-                try {
-                  final loggedUser = await _auth.signInWithEmailAndPassword(
-                      email: email, password: password);
-                  if (loggedUser != null) {
-                    Navigator.pushNamed(context, ChatScreen.id);
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              HeroLogo(imageHeight: 250),
+              SizedBox(height: 48.0),
+              EmailTextField(onChangedValue: (value) {
+                email = value;
+              }),
+              SizedBox(height: 8.0),
+              PasswordTextField(onChangedValue: (value) {
+                password = value;
+              }),
+              SizedBox(height: 24.0),
+              BottonsLogin(
+                function: () async {
+                  setState(() {
+                    showSpinner = true;
+                  });
+                  try {
+                    final loggedUser = await _auth.signInWithEmailAndPassword(
+                        email: email, password: password);
+                    if (loggedUser != null) {
+                      Navigator.pushNamed(context, ChatScreen.id);
+                    }
+                  } catch (e) {
+                    print(e);
                   }
-                } catch (e) {
-                  print(e);
-                }
-              },
-              buttonText: 'Log in',
-              color: Colors.blueAccent,
-            ),
-          ],
+                  setState(() {
+                    showSpinner = false;
+                  });
+                },
+                buttonText: 'Log in',
+                color: Colors.blueAccent,
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -5,6 +5,7 @@ import 'package:flash_chat_flutter/widgets/email_text_field.dart';
 import 'package:flash_chat_flutter/widgets/hero_logo.dart';
 import 'package:flash_chat_flutter/widgets/password_text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static const String id = 'register_screen';
@@ -14,43 +15,53 @@ class RegistrationScreen extends StatefulWidget {
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
   final _auth = FirebaseAuth.instance;
+  bool showSpinner = false;
   String email;
   String password;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            HeroLogo(imageHeight: 250),
-            SizedBox(height: 48.0),
-            EmailTextField(onChangedValue: (value) {
-              email = value;
-            }),
-            SizedBox(height: 8.0),
-            PasswordTextField(onChangedValue: (value) {
-              password = value;
-            }),
-            SizedBox(height: 24.0),
-            BottonsLogin(
-              function: () async {
-                try {
-                  final newUser = await _auth.createUserWithEmailAndPassword(
-                      email: email, password: password);
-                  if (newUser != null) {
-                    Navigator.pushNamed(context, ChatScreen.id);
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              HeroLogo(imageHeight: 250),
+              SizedBox(height: 48.0),
+              EmailTextField(onChangedValue: (value) {
+                email = value;
+              }),
+              SizedBox(height: 8.0),
+              PasswordTextField(onChangedValue: (value) {
+                password = value;
+              }),
+              SizedBox(height: 24.0),
+              BottonsLogin(
+                function: () async {
+                  setState(() {
+                    showSpinner = true;
+                  });
+                  try {
+                    final newUser = await _auth.createUserWithEmailAndPassword(
+                        email: email, password: password);
+                    if (newUser != null) {
+                      Navigator.pushNamed(context, ChatScreen.id);
+                    }
+                  } catch (e) {
+                    print(e);
                   }
-                } catch (e) {
-                  print(e);
-                }
-              },
-              buttonText: 'Register',
-              color: Colors.blueAccent,
-            ),
-          ],
+                  setState(() {
+                    showSpinner = false;
+                  });
+                },
+                buttonText: 'Register',
+                color: Colors.blueAccent,
+              ),
+            ],
+          ),
         ),
       ),
     );
